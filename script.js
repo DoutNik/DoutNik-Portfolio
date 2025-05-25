d9276674e0c013155dc85bee137a899e983fc6c0
Carlos Emanuel Klema <carlosklema@hotmail.com>
2025-05-25 19:52:01 -0300

    CV Translation "es"-"pt"

--- script.js
+++ script.js
@@ -37,6 +37,29 @@ function efectoHabilidades(){
 }
 
 
+const switcher = document.getElementById("languageSwitcher");
+const elements = document.querySelectorAll("[data-i18n]");
+
+function setLanguage(lang) {
+  elements.forEach(el => {
+    const key = el.getAttribute("data-i18n");
+    if (translations[lang] && translations[lang][key]) {
+      el.textContent = translations[lang][key];
+    }
+  });
+}
+
+const defaultLang = localStorage.getItem("lang") || "en";
+setLanguage(defaultLang);
+switcher.value = defaultLang;
+
+switcher.addEventListener("change", () => {
+  const selectedLang = switcher.value;
+  localStorage.setItem("lang", selectedLang);
+  setLanguage(selectedLang);
+});
+
+
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
